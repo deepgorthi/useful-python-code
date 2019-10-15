145475a85834fce6e4e1b79e0cee2c36a2bfa4b2
Pradeep <deep.gorthi@gmail.com>
2019-10-15 14:22:23 -0400

    Create unique-elems-in-string.py

---
+++ unique-elems-in-string.py
@@ -0,0 +1,12 @@
+my_string = "aabbbbbbccccddddeee"
+
+# converting the string to a set
+temp_set = set(my_string)
+
+# all elements in a set are unique.
+
+# stitching set into a string using join
+new_string = ''.join(temp_set)
+
+print(new_string)
+
